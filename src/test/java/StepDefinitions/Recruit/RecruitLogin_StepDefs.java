dbbb9e9a5435125bb0ffe34ae765a66ba829bb25
sagar yadav <sagar.yadav@gmail.com>
2018-08-14 10:53:41 +0100

    Created CRM Recruit Login Feature File

---
+++ src/test/java/StepDefinitions/Recruit/RecruitLogin_StepDefs.java
@@ -0,0 +1,12 @@
+package StepDefinitions.Recruit;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
+public class RecruitLogin_StepDefs {
+    @Given("^I am on ICL CRM Recruit login page$")
+    public void iAmOnICLCRMRecruitLoginPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
