a9418fffacbde0a0d928f18521e17681f2a0cadf
Sagar Yadav <sagar.yadav@imperial.ac.uk>
2018-11-27 14:42:07 +0000

    Added Student Login Logout functionality

---
+++ src/test/java/Pages/WebFrontEnd/LoginLogout.java
@@ -0,0 +1,70 @@
+package Pages.WebFrontEnd;
+
+import BaseFramework.Plumbing.Driver_Init;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import sun.awt.windows.WEmbeddedFrame;
+
+public class LoginLogout extends Driver_Init{
+
+
+    public LoginLogout(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = ".//input[@id='Username']")
+    public static WebElement userName_TextField;
+
+    @FindBy(xpath = ".//input[@id='Password']")
+    public static WebElement password_TextField;
+
+    @FindBy(xpath = ".//input[@id='submitLogin']")
+    public static WebElement login_Button;
+
+    @FindBy(xpath = ".//li[@class='active']/a")
+    public static WebElement getVerification_Text;
+
+    @FindBy(xpath = ".//span[@class='user-name']")
+    public static WebElement userName_Link;
+
+    @FindBy(xpath = ".//form[@id='logoutForm']")
+    public static WebElement logout_Button;
+
+
+    public void enterStudentUserName(){
+        userName_TextField.sendKeys("Derick.Kirby@mailinator.com");
+    }
+
+    public void enterStudentPassword(){
+        password_TextField.sendKeys("Test123!");
+    }
+
+    public void clickOnLoginButton(){
+        login_Button.click();
+    }
+
+    public void verifyLoginText(String loginBodyText){
+        String actualText = getVerification_Text.getText();
+        Assert.assertEquals(loginBodyText,actualText);
+    }
+
+    public void loginAsStudent(){
+        userName_TextField.sendKeys();
+        password_TextField.sendKeys();
+        login_Button.click();
+    }
+
+    public void logoutAsStudent(){
+        userName_Link.click();
+        logout_Button.click();
+    }
+
+    public void verifyLogoutText(String logoutBodyText){
+        String actualText = getVerification_Text.getText();
+        Assert.assertEquals(logoutBodyText,actualText);
+    }
+
+}
---
+++ src/test/resources/Features/WebFrontEnd/08_Student_LoginLogout.feature
@@ -0,0 +1,12 @@
+@Monitor
+Feature: Login and Logout as a Student
+
+  Scenario: Login Logout as a Student
+    Given I am on ICL gateway web page
+    And I enter my username
+    And I enter my password
+    And I click on Log in button
+    And I should get "MY ACCOUNT"
+    When I click on Logout
+    Then I should get "LOG IN" option
+
